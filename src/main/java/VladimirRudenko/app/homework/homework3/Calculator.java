8d1dc9f7f3f8e1bb68ef9b809c69ad4b72e70b86
vladimir1rudenko@gmail.com <vladimir1rudenko@gmail.com>
2017-02-02 09:51:08 +0200

    first 5 unit tests

---
+++ src/main/java/VladimirRudenko/app/homework/homework3/Calculator.java
@@ -0,0 +1,18 @@
+package VladimirRudenko.app.homework.homework3;
+
+/**
+ * Created by vladimir on 30.01.17.
+ */
+
+public class Calculator {
+
+    public double add(double y, double x) {return x + y;}
+
+    public double deduct(double y, double t) {return  y - t;}
+
+    public double mult(double x, double y) {return x * y;}
+
+    public double del(double x, double y) {return x / y;}
+
+    public double sqrt(double f) {return f *f;}
+}
--- src/test/java/VladimirRudenko/app/homework/homework3/CalculatorTest.java
+++ src/test/java/VladimirRudenko/app/homework/homework3/CalculatorTest.java
@@ -11,12 +11,12 @@ import static org.junit.Assert.*;
  * Created by vladimir on 30.01.17.
  */
 
-/*public class MathTest {
+public class MathTest {
     @Test
     public void max()  {
-        Math math = new Math();
+        Math max = new Math();
         Assert.assertEquals(6, Math.max(6, 5));
 
     }
 
-}*/
+}
