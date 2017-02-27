b388a9da1eda5b593070357dc98c7c38b78522cc
vladimir1rudenko@gmail.com <vladimir1rudenko@gmail.com>
2017-02-27 17:47:48 +0200

    Added Runner

--- src/main/java/VladimirRudenko/runners/Lesson12Runner.java
+++ src/main/java/VladimirRudenko/runners/Lesson12Runner.java
@@ -1,7 +1,23 @@
 package VladimirRudenko.runners;
 
+import VladimirRudenko.app.homework.lesson12.Human;
+
 /**
  * Created by vladimir on 27.02.17.
  */
 public class Lesson12Runner {
+    public static void main(String[] args) {
+        Human human = new Human(27,"Vova");
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+        human.setAge(29);
+
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+        human.setAge(121);
+
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+
+    }
 }
