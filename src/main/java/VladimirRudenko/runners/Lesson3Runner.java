588ca4a5f2205f4feed79a0a47446b14dcfd141b
vladimir1rudenko@gmail.com <vladimir1rudenko@gmail.com>
2017-02-27 17:49:46 +0200

    lesson3

--- src/main/java/VladimirRudenko/runners/Lesson3Runner.java
+++ src/main/java/VladimirRudenko/runners/Lesson3Runner.java
@@ -1,7 +1,12 @@
 package VladimirRudenko.runners;
 
+import VladimirRudenko.app.classwork.lesson3.MathFunc;
+
 /**
  * Created by vladimir on 20.02.17.
  */
 public class Lesson3Runner {
+    public static void main(String[] args) {
+        MathFunc.add();
+    }
 }
