1166de91a175520945c1ac31fb1a49b55dbdd494
vladimir1rudenko@gmail.com <vladimir1rudenko@gmail.com>
2017-03-06 20:50:13 +0200

    runner

--- src/main/java/VladimirRudenko/app/classwork/lesson15/Run.java
+++ src/main/java/VladimirRudenko/app/classwork/lesson15/Run.java
@@ -4,4 +4,13 @@ package VladimirRudenko.app.classwork.lesson15;
  * Created by vladimir on 06.03.17.
  */
 public class Run {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        Parent vi = new Child1();
+        Parent cv = new Child2();
+        parent.say();
+        vi.say();
+        cv.say();
+
+    }
 }
